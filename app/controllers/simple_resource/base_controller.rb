6edfa34e8eb3e79e9ddabe07b4a7cac6a8b67377
Jari Jokinen <info@jarijokinen.com>
2012-10-21 23:58:30 +0300

    Redirect to collection_url after create and update

--- app/controllers/simple_resource/base_controller.rb
+++ app/controllers/simple_resource/base_controller.rb
@@ -2,5 +2,13 @@ module SimpleResource
   class BaseController < ::ApplicationController
     inherit_resources
     defaults route_prefix: ""
+
+    def create
+      create! { collection_url }
+    end
+        
+    def update
+      update! { collection_url }
+    end
   end
 end
--- spec/integration/languages_spec.rb
+++ spec/integration/languages_spec.rb
@@ -1,13 +1,13 @@
 require "spec_helper"
 
-describe "Languages" do
-  describe "#index" do
-    let!(:collection) { FactoryGirl.create_list(:language, 10) }
+describe "Languages resource" do
+  let!(:collection) { FactoryGirl.create_list(:language, 10) }
+  
+  before :each do
+    visit "/languages"
+  end
 
-    before :each do
-      visit "/languages"
-    end
-
+  describe "index" do
     it "has a correct title" do
       page.should have_xpath "//title", text: "Languages | Dummy"
     end
@@ -34,6 +34,45 @@ describe "Languages" do
 
     it "has a link to create a new language" do
       page.should have_link "New Language", href: "/languages/new"
+    end
+  end
+
+  describe "creating a new language" do
+    let(:resource) { FactoryGirl.build(:language) }
+
+    before :each do
+      click_link "New Language"
+    end
+    
+    it "has a correct title" do
+      page.should have_xpath "//title", text: "New Language | Dummy"
+    end
+
+    it "has a correct heading" do
+      page.should have_css "h1", "New Language"
+    end
+
+    it "has a name field" do
+      page.should have_field "Name"
+    end
+
+    it "has a create language button" do
+      page.should have_button "Create Language"
+    end
+
+    it "has a cancel link" do
+      page.should have_link "Cancel", href: "/languages"
+    end
+
+    context "with correct values" do
+      before :each do
+        fill_in "Name", with: resource.name
+        click_button "Create Language"
+      end
+
+      it "redirects to languages index" do
+        page.current_path.should == "/languages"
+      end
     end
   end
 end
