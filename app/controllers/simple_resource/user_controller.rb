ca22342438da7e02d3f682c1df1ba075213f699c
Jari Jokinen <info@jarijokinen.com>
2013-01-17 11:46:21 +0200

    Fix

--- app/controllers/simple_resource/user_controller.rb
+++ app/controllers/simple_resource/user_controller.rb
@@ -1,10 +1,12 @@
 module SimpleResource
   class UserController < SimpleResource::BaseController
     protected
-
+  
     def begin_of_association_chain
       if defined?(Devise)
         current_user
+      else
+        nil
       end
     end
   end
